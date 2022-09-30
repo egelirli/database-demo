506efae9b66c6045078b24314efcf62817023cfe
egelirli <egelirli@yahoo.com>
2022-09-30 23:54:34 +0300

    initial

---
+++ src/main/java/com/egelirli/spring/databese/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,67 @@
+package com.egelirli.spring.databese.databasedemo.jdbc;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.egelirli.spring.databese.databasedemo.entity.Person;
+
+@Repository
+public class PersonJdbcDao {
+  
+		@Autowired
+	    private JdbcTemplate jdbcTemplate; 
+	    
+	    public List<Person>  findAll(){
+	    	
+	    	return jdbcTemplate.query("Select * from Person", 
+	    							new BeanPropertyRowMapper<Person>(Person.class));
+	    	
+	    }
+
+	    public Person  findPersonById(int id){
+	    	
+	    	return jdbcTemplate.queryForObject("Select * from Person Where id=?",
+	    							new Object[] { id },
+	    							new BeanPropertyRowMapper<Person>(Person.class));
+	    	
+	    }
+	    
+
+	    public int  deletePersonById(int id){
+	    	
+	    	return jdbcTemplate.update("delete from Person Where id=?",
+	    												new Object[] { id });
+	    	
+	    }
+
+	    
+	    public int  insertPerson(Person person){
+	    	return jdbcTemplate.update(
+   						"Insert Into Person(id, name, location, birth_date) "
+			                      + " valuse(?, ? ,? ,?);",
+    									new Object[] { 
+    											person.getId(), 
+    											person.getName(), 
+     										    person.getLocation(), 
+     										    new Timestamp(person.getBirthDate().getTime()) });
+	    } 
+
+	    public int  updatePerson(Person person){
+	    	return jdbcTemplate.update(
+   						"update Person "
+			                      + " set name = ?, set location = ?, set birth_date = ? "
+			                      + " wehere  id = ? ",
+    									new Object[] {  
+    											person.getName(), 
+    											person.getLocation(), 
+    											new Timestamp(person.getBirthDate().getTime()),
+    											person.getId() 
+    										});
+	    } 
+
+}
